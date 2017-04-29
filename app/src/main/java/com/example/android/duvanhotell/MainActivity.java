cb82124338560d6c9c54cf5cb88d6217359224e2
Sumedha Dharmasiri <Sumedha.Dharmasiri@gmail.com>
2017-04-29 11:33:05 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/duvanhotell/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.android.duvanhotell;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.text.method.MovementMethod;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
